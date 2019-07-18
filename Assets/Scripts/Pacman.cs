3a2e92730c67d12a8bd2ce32e009086c3a5d94c6
KJeffree <k.jeffree@gmail.com>
2019-07-18 19:31:19 +0100

    ghosts added, pacman moves

---
+++ Assets/Scripts/Pacman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacman : MonoBehaviour
+{
+        float speed = 10.00f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * speed;
+        deltaX *= Time.deltaTime;
+        transform.Translate(deltaX, 0, 0);
+    }
+}
