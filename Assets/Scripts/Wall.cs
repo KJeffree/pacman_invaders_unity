14ad284abb3944f1a127082e251488deb5ba0f49
Craig Morton <CraigMorton22@gmail.com>
2019-07-19 16:18:47 +0100

    Add invisible walls to left and right edges of scene

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var collisionX = collision.GetContact(0).point.x;
+        Debug.Log("Wall collision enter at xPos: " + collisionX);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+    }
+}
