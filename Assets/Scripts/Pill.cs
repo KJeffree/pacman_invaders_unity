d45d4b90a3a3847ea8abdf3f52609d46cc38ecab
Jen Proctor <jen_p_13@hotmail.co.uk>
2019-07-21 15:30:15 +0100

    pill moves up the screen and destroys self

--- Assets/Scripts/Pill.cs
+++ Assets/Scripts/Pill.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Pill : MonoBehaviour
 {
+    private Transform pill;
+    float speed = 0.30f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pill = GetComponent<Transform>();
+    }
+
+    // Move every set period of time, instead of every frame
+    void FixedUpdate()
+    {
+        pill.position += Vector3.up * speed;
+
+        if (pill.position.y >= 10)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.tag == "Ghost")
+        //{
+        //   Debug.Log("Hit a ghost!");
+        //    Destroy(collision.gameObject);
+        //}
     }
 }
