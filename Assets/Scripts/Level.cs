8b4d4cf2ef9147986132c23ffbf978a9bd980c5f
KJeffree <k.jeffree@gmail.com>
2019-07-22 10:25:43 +0100

    pacman has 3 lives and lives displayed on screen

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] int lives = 3;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLivesText();
+    }
+
+    public void LoseLife()
+    {
+        lives -= 1;
+    }
+
+    private void UpdateLivesText()
+    {
+        livesText.text = lives.ToString();
+    }
+}
