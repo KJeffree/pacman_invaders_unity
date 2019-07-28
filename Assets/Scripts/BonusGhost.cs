c49af00e066f034552759715f0fae7eb470df3cb
Jen Proctor <jen_p_13@hotmail.co.uk>
2019-07-28 11:58:46 +0100

    added bonus ghost sprite and script

---
+++ Assets/Scripts/BonusGhost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusGhost : MonoBehaviour
+{
+    private Rigidbody2D bonusGhost;
+    float speed = 5.00f;
+
+    void Start()
+    {
+        bonusGhost = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        bonusGhost.velocity = new Vector2(speed, 0);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
