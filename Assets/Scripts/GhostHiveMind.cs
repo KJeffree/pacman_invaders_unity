15091854e97c875c243e1a4ab593898c0da56190
Katie Jeffree <k.jeffree@gmail.com>
2019-08-27 15:24:31 +0100

    returned fruit drop to normal

--- Assets/Scripts/GhostHiveMind.cs
+++ Assets/Scripts/GhostHiveMind.cs
@@ -14,12 +14,12 @@ public class GhostHiveMind : MonoBehaviour
     {
         Level level = FindObjectOfType<Level>();
         int i=0;
-        int amount = 44;
+        int amount = 4;
         while (i < amount)
         {
             int ghostIndex = Random.Range(0, ghosts.Count-1);
             int index = Random.Range(0, Mathf.Clamp(i, 0, level.GetWaveNumber()) + 1);
-            ghosts[ghostIndex].SetFruit(fruits[2]);
+            ghosts[ghostIndex].SetFruit(fruits[index]);
             i++;
         }
     }
